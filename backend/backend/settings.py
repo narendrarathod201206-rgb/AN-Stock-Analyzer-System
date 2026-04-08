b670bf0b0edfc48ba9cce097d97bc61c5b5bdb76
Narendra <narendra.rathod201206@gmail.com>
2026-04-08 16:13:42 +0530

    Fix production settings and database issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,13 +8,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4t#j_htk)02381g2c)r$_2uayp4aibuoz%52fun5_3knbvt6j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['an-stock-analyzer-system.onrender.com', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.loca.lt',
     'https://*.pythonanywhere.com',
+    'https://an-stock-analyzer-system.onrender.com',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
